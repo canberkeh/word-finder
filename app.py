2503084fd6a6f4d0737ef1606d041b7f16ed97a9
berke-horozal-crea <90218881+berke-horozal-crea@users.noreply.github.com>
2022-02-06 17:06:38 +0300

    Set port for flask app

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from services.base_service import BaseService
 from dotenv import load_dotenv
 from pathlib import Path
@@ -10,4 +11,5 @@ import controllers
 from controllers import app
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host="0.0.0.0", port=port)
