4f0d53627c1329020712575f9021e881fc5bd41e
berke-horozal-crea <90218881+berke-horozal-crea@users.noreply.github.com>
2022-02-12 20:07:37 +0300

    Minor fix in service

--- test.py
+++ test.py
@@ -1,9 +1,16 @@
 import json
 import requests
 headers = {'content-type': 'application/json'}
-post_data = {'length' : "5", "word": "s   m" }
+post_data = {"exclude": [
+    "c",
+    "z"
+  ],
+  "include": [
+    "a",
+    "s"
+  ],'length' : "5", "word": "s   m", "language": "tr"}
 
 # response = requests.post("http://word-finder-get-words.herokuapp.com/word_finder/get_words", data=json.dumps(post_data), headers=headers, auth=("word_finder", "wordie1993@"))
-response = requests.post("http://localhost:5000/word_finder/get_words", data=json.dumps(post_data), headers=headers, auth=("word_finder", "wordie1993@"))
+response = requests.post("http://word-finder-get-words.herokuapp.com/word_finder/get_words", data=json.dumps(post_data), headers=headers, auth=("word_finder", "wordie1993@"))
 
 print(response.content)
